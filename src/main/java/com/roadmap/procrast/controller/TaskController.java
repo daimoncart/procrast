57fc70fdc89e6d71cd0dc6ea9b506496801e213d
daimoncart <raimonds.liepins@gmail.com>
2021-04-28 22:48:57 +0300

    add crossorigin annotation

--- src/main/java/com/roadmap/procrast/controller/TaskController.java
+++ src/main/java/com/roadmap/procrast/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.roadmap.procrast.controller;
 import com.roadmap.procrast.model.TaskDTO;
 import com.roadmap.procrast.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TaskController {
 
